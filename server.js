9da01cc8ea2b74337594e928df0be5ed1e53cb86
rkgallaway <rkgallaway@msn.com>
2018-11-16 17:26:46 -0800

    class deployment

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ const PORT = 3000;
 app.use(express.static('./public'));
 
 //Tell the app to listen so that we will serve files from
-app.listen(PORT, () => console.log(`Our app is listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Our app is listening on port ${PORT}`));
